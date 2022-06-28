fdd7755b74785dc034b896c035c785a2a9e87bb0
Salvador <salvadorestadistic@gmail.com>
2022-06-29 08:52:33 +0900

    Agregue la codificacion para la variable 3

--- Graficas_analizis.R
+++ Graficas_analizis.R
@@ -12,18 +12,6 @@ DBT=read_csv2("DB_WS1.csv")
 attach(DBT)
 DBT$V10[1]="Very true"
 
-Cod1<-function(A){
-  for (i in 1:length(A)) {
-    if(A[i]=="Male"){A[i]=1} else {A[i]=2};
-  }
-  A=as.numeric(A)
-}
-Genre=Cod1(V2)
-Genre
-pie(table(Genre))
-
-table(V2)
-
 d1=data.frame(table(V2))
 d2=data.frame(x=d1$V2,y=d1$Freq)
 d2$y<-round(d2$y/length(V2)*100,digits = 1)
@@ -44,4 +32,25 @@ ggplot(d2, aes(x="",y=y,fill=x)) + geom_col(color = "black")+
         panel.background = element_rect(fill = "#ffffff"),
         plot.background = element_rect(fill = "#ffffff"),
         legend.background = element_rect(fill = "#ffffff"))
+
+#Valores (1) Very true, (2) Not very true, (3) Sort of true, (4) Not at all true
+
+Cod<-function(A){
+ch
+  for (i in 1:length(A)) {
+    ch=switch(A[i], "Very true" = 1, "Not very true"=2,
+              + "Sort of true"=3, "Not at all true"=4)
+  }
+  ch
+}
+NV3=Cod(V3)
+
+
+Genre
+
+
+
+
+
+
 
